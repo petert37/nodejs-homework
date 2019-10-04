b2b4ae13f87ab6c95d5a9c9b7e3b9142824de735
Tóth Péter <toth.peter37@gmail.com>
2019-10-04 15:30:06 +0200

    added html views with navigation

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 
 app.use(express.static("static"));
 
+app.get('/', (req, res) => res.redirect("login.html"));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Servers listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
